0fff7839d6cdb965c8f189b95bf04cab70fb0fc3
Marshall Thompson <marshall@creativeideal.net>
2016-03-23 03:03:21 -0600

    1.0.2

---
+++ dist/amd/next-record.js
@@ -0,0 +1,8 @@
+/*next-record@1.0.1#next-record*/
+define([
+    'exports',
+    './next/next',
+    './previous/previous'
+], function (exports, _next, _previous) {
+    'use strict';
+});
---
+++ dist/cjs/next-record.js
@@ -0,0 +1,4 @@
+/*next-record@1.0.1#next-record*/
+'use strict';
+require('./next/next.js');
+require('./previous/previous.js');
